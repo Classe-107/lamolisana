6094b4935e4400722ee4c0496e9be9098255fce8
klelia <clelia.fradella@gmail.com>
2024-01-05 12:23:17 +0100

    fix template prooperty name

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -10,10 +10,10 @@
           @foreach ($products as $key => $product)
             <div class="col-12 col-md-4 col-lg-3">
                 <div class="card">
-                    <img src="{{$product['src']}}" alt="{{$product['titolo']}}" class="card-img-top">
+                    <img src="{{$product['image']}}" alt="{{$product['title']}}" class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title">{{$product['titolo']}}</h5>
-                        <p class="card-text">{!! substr($product['descrizione'], 0, 100) . '...' !!}</p>
+                        <h5 class="card-title">{{$product['title']}}</h5>
+                        <p class="card-text">{!! substr($product['description'], 0, 100) . '...' !!}</p>
                         <a href="{{route('products.show', $key)}}" class="btn btn-primary">Vedi dettaglio</a>
                     </div>
                 </div>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -10,12 +10,12 @@
 
             <div class="col-12">
                 <div class="card">
-                    <img src="{{$product['src']}}" alt="{{$product['titolo']}}" class="card-img-top">
+                    <img src="{{$product['image']}}" alt="{{$product['title']}}" class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title">{{$product['titolo']}}</h5>
-                        <p class="card-text">{!! $product['descrizione'] !!}</p>
+                        <h5 class="card-title">{{$product['title']}}</h5>
+                        <p class="card-text">{!! $product['description'] !!}</p>
                         <p>
-                          Peso: {{$product['peso']}} | Cottura: {{$product['cottura']}} | Tipo: {{$product['tipo']}}
+                          Peso: {{$product['weight']}} | Cottura: {{$product['cooking_time']}} | Tipo: {{$product['type']}}
                         </p>
                     </div>
                 </div>
